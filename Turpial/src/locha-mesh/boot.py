87fe6c927ff063cc67c20fa945b9e69df48715a9
luisan00 <luisan00@hotmail.com>
2019-02-02 17:05:09 +0100

    updated boot.py, main.py

---
+++ Turpial/src/locha-mesh/boot.py
@@ -0,0 +1,8 @@
+"""
+(c) Copyright 2019 locha.io developers
+Licensed under a MIT license, see LICENSE file in the root folder
+for the full text.
+"""
+# run when device boot up or wake from sleep.
+
+
