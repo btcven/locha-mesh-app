2a8f56fb1c622a3ceaf09ab713bc5e406a082dca
luisan00 <luisan00@hotmail.com>
2019-02-20 00:53:42 +0100

    Update bluetooth.ino

--- Turpial/bluetooth.ino
+++ Turpial/bluetooth.ino
@@ -1,4 +1,8 @@
 /**
+ * Based on Neil Kolban example for IDF: https://github.com/nkolban/esp32-snippets/blob/master/cpp_utils/tests/BLE%20Tests/SampleNotify.cpp
+ * Ported to Arduino ESP32 by Evandro Copercini
+ */
+/**
  * (c) Copyright 2019 locha.io project developers
  * Licensed under a MIT license, see LICENSE file in the root folder
  * for a full text.
