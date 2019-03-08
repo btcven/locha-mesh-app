97758f6aaed70a5e93dbffb69cb180b9d85d4b5c
luisan00 <luisan00@hotmail.com>
2019-03-08 09:32:12 +0100

    fix broken dependencies to BLE library

--- Turpial/bluetooth.cpp
+++ Turpial/bluetooth.cpp
@@ -7,17 +7,17 @@
  * Licensed under a MIT license, see LICENSE file in the root folder
  * for a full text.
  */
-// #include <cstdio>
+//#include <cstdio>
 //#include <cstdlib>
 //#include <string>
 #include <Arduino.h>
-#include <BLEDevice.h>
-#include <BLEServer.h>
-#include <BLEUtils.h>
-#include <BLE2902.h>
+#include <lib/ESP32_BLE_Arduino/src/BLEDevice.h>
+#include <lib/ESP32_BLE_Arduino/src/BLEServer.h>
+#include <lib/ESP32_BLE_Arduino/src/BLEUtils.h>
+#include <lib/ESP32_BLE_Arduino/src/BLE2902.h>
 #include "defaultStartup.h"
 
-extern BLE_status_t  BLEStatus;
+extern BLE_status_t BLEStatus;
 
 BLEServer *pServer = NULL;
 BLECharacteristic *pTxCharacteristic;
@@ -28,40 +28,49 @@ String rxValue;
 bool deviceConnected = false;
 bool oldDeviceConnected = false;
 
-#define SERVICE_UUID            "6E400001-B5A3-F393-E0A9-E50E24DCCA9E" // Service UUID
-#define CHARACTERISTIC_UUID_RX  "6E400002-B5A3-F393-E0A9-E50E24DCCA9E" // Characteristic RX
-#define CHARACTERISTIC_UUID_TX  "6E400003-B5A3-F393-E0A9-E50E24DCCA9E" // Characteristic TX
+#define SERVICE_UUID "6E400001-B5A3-F393-E0A9-E50E24DCCA9E"           // Service UUID
+#define CHARACTERISTIC_UUID_RX "6E400002-B5A3-F393-E0A9-E50E24DCCA9E" // Characteristic RX
+#define CHARACTERISTIC_UUID_TX "6E400003-B5A3-F393-E0A9-E50E24DCCA9E" // Characteristic TX
 
-class ServerCB : public BLEServerCallbacks {
-    void onConnect(BLEServer *pServer) {
-      deviceConnected = true;
-    }
+class ServerCB : public BLEServerCallbacks
+{
+  void onConnect(BLEServer *pServer)
+  {
+    deviceConnected = true;
+    BLEStatus.isConnected = true;
+  }
 
-    void onDisconnect(BLEServer *pServer) {
-      deviceConnected = false;
-    }
+  void onDisconnect(BLEServer *pServer)
+  {
+    deviceConnected = false;
+    BLEStatus.isConnected = false;
+  }
 };
 
-class characteristicCB : public BLECharacteristicCallbacks {
-  
-    void onWrite(BLECharacteristic *pCharacteristic) {
-     
-      rxValue = pCharacteristic->getValue();
+class characteristicCB : public BLECharacteristicCallbacks
+{
+
+  void onWrite(BLECharacteristic *pCharacteristic)
+  {
+
+    rxValue = pCharacteristic->getValue();
 
     //  if (rxValue.size() > 0) {
-        if (rxValue.length() > 0) {
-        
-        Serial.println(rxValue.c_str());
-        // incoming msg via ble iface:
-        // 1.- send via rad iface handler
-        // 2.- clear the var.
-        //rxValue.clear();
-        rxValue="";
-      }
+    if (rxValue.length() > 0)
+    {
+
+      Serial.println(rxValue.c_str());
+      // incoming msg via ble iface:
+      // 1.- send via rad iface handler
+      // 2.- clear the var.
+      //rxValue.clear();
+      rxValue = "";
     }
+  }
 };
 
-void bluetooth_task(void *params) {
+void bluetooth_task(void *params)
+{
 
   // device name must be: mac_address + ".locha.io"
   // not yet implemented.
@@ -71,13 +80,13 @@ void bluetooth_task(void *params) {
   pServer->setCallbacks(new ServerCB());
   // Can be included in the ServerCallback)?
   BLEStatus.isActive = true;
-  
+
   // Set up the BLE Service
   BLEService *pService = pServer->createService(SERVICE_UUID);
   // And assign it BLE Characteristics
-  pTxCharacteristic = pService->createCharacteristic(CHARACTERISTIC_UUID_TX,BLECharacteristic::PROPERTY_NOTIFY);
+  pTxCharacteristic = pService->createCharacteristic(CHARACTERISTIC_UUID_TX, BLECharacteristic::PROPERTY_NOTIFY);
   pTxCharacteristic->addDescriptor(new BLE2902());
-  BLECharacteristic *pRxCharacteristic = pService->createCharacteristic(CHARACTERISTIC_UUID_RX,BLECharacteristic::PROPERTY_WRITE);
+  BLECharacteristic *pRxCharacteristic = pService->createCharacteristic(CHARACTERISTIC_UUID_RX, BLECharacteristic::PROPERTY_WRITE);
   pRxCharacteristic->setCallbacks(new characteristicCB());
   // Start the service
   pService->start();
@@ -86,29 +95,34 @@ void bluetooth_task(void *params) {
   pServer->getAdvertising()->start();
   Serial.printf("Start advertising\n");
 
-  while (1) {
-    if (deviceConnected) {
-  //    if (txValue.size() > 0) {
-   if (txValue.length() > 0) {
-  
+  while (1)
+  {
+    if (deviceConnected)
+    {
+      //    if (txValue.size() > 0) {
+      if (txValue.length() > 0)
+      {
         pTxCharacteristic->setValue(txValue);
         pTxCharacteristic->notify();
-     //  txValue.clear();
-        txValue="";
+        //  txValue.clear();
+        txValue = "";
       }
       delay(10);
     }
-    else {
-     // if (txValue.size() > 0) {
-        if (txValue.length() > 0) {
+    else
+    {
+      // if (txValue.size() > 0) {
+      if (txValue.length() > 0)
+      {
         Serial.printf("Device not connected to BLE interface.\n");
         Serial.printf("Received: ");
-        Serial.println(txValue.c_str());
+        Serial.println(txValue);
         //txValue.clear();
-        txValue="";
+        txValue = "";
       }
       // disconnecting
-      if (!deviceConnected && oldDeviceConnected) {
+      if (!deviceConnected && oldDeviceConnected)
+      {
         // Give the Bluetooth interface a chance to get ready
         delay(500);
         // And restart advertising
@@ -117,7 +131,8 @@ void bluetooth_task(void *params) {
         oldDeviceConnected = deviceConnected;
       }
       // connecting
-      if (deviceConnected && !oldDeviceConnected) {
+      if (deviceConnected && !oldDeviceConnected)
+      {
         //
         oldDeviceConnected = deviceConnected;
       }
