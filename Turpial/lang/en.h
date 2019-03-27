ec3c831cfb86950cc861783ea7385676e5762273
luisan00 <luisan00@hotmail.com>
2019-03-27 12:14:13 +0100

    added folder for languages defs.

---
+++ Turpial/lang/en.h
@@ -0,0 +1,12 @@
+/**
+ * @Copyright:
+ * (c) Copyright 2019 locha.io project developers
+ * Licensed under a MIT license, see LICENSE file in the root folder
+ * for a full text
+ */
+
+#ifndef EN_H
+#define EN_H
+
+
+#endif // EN_H
