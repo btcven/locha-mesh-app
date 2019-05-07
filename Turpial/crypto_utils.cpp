5b3c1505446ce4b661d8a3e40a933e0edb590415
luisan00 <luisan00@hotmail.com>
2019-05-07 18:07:38 +0200

    added checksum and hashing functions

--- Turpial/crypto_utils.cpp
+++ Turpial/crypto_utils.cpp
@@ -11,12 +11,12 @@
  */
 
 #include <Arduino.h>
-#include <Bitcoin.h>
-#include <Hash.h>
+#include "Hash.h"
+
 #include "crypto_utils.h"
 #include "general_utils.h"
 #include "memory_def.h"
-
+/*
 // esta funcion verifica si el hash del mensaje es valido comparando el hash160
 bool is_valid_hash160(char *mensaje, char *hash_recibido)
 {
@@ -39,4 +39,47 @@ std::string hash160(char *mensaje)
   byte hash[SIZE_HASH_MSG] = {0};
   hash160(mensaje, sizeof(mensaje), hash);
   return (ToString(hash));
+}
+
+*/
+
+
+/**
+ * @brief 
+ * 
+ * @param buff 
+ * @return uint8_t* 
+ */
+uint8_t *hash(const char *buff)
+{
+  uint8_t hash[32];
+  int result = sha256(buff, sizeof(buff), hash);
+  if (result)
+    return hash;
+}
+
+
+/**
+ * @brief 
+ * 
+ * @param buff 
+ * @param sub 
+ * @return uint8_t* 
+ */
+uint8_t *checksum(const char *buff, size_t sub)
+{
+  uint8_t *h = hash(buff);
+  uint8_t result[sub];
+  for (size_t i = 0; i < sub; i++)
+  {
+    result[i] = h[i];
+  }
+  return result;
+}
+
+bool checksum_compare(const char *buff, uint8_t *sum) {
+  uint8_t *result = checksum(buff, sizeof(sum));
+  // compare between two uint8_t pointers.
+
+
 }
