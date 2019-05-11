3d0337a042f798cb64ba6fce00597590564556fa
luisan00 <luisan00@hotmail.com>
2019-05-12 00:31:37 +0200

    working on ble

--- Turpial/BLE.cpp
+++ Turpial/BLE.cpp
@@ -8,6 +8,75 @@
  * @copyright Copyright (c) 2019 locha.io project developers
  * 
  */
-
-#include <Arduino.h>
+#include <BLEServer.h>
+#include <BLEDevice.h>
+#include <BLEUtils.h>
+#include <BLE2902.h>
 #include "BLE.h"
+
+BLEServer *ble_server = NULL;
+BLECharacteristic *tx_uart;
+BLECharacteristic *rx_uart;
+bool deviceConnected = false;
+
+class ServerCB : public BLEServerCallbacks
+{
+    void onConnect(BLEServer *ble_server)
+    {
+        deviceConnected = true;
+        ESP_LOGD("BLE", "Client connected");
+    }
+    void onDisconnect(BLEServer *ble_server)
+    {
+        deviceConnected = false;
+        ESP_LOGD("BLE", "Client disconnected");
+        delay(100); // was 500
+        ble_server->startAdvertising();
+    }
+};
+
+class characteristicCB : public BLECharacteristicCallbacks
+{
+    void onWrite(BLECharacteristic *pCharacteristic)
+    {
+        // determinar *pCharacteristic
+    }
+    void onRead(BLECharacteristic *pCharacteristic)
+    {
+        // determinar *pCharacteristic
+    }
+};
+
+void BLE_task(void *params)
+{
+
+    BLEDevice::setMTU(server_mtu);
+    BLEDevice::init(server_name);
+
+    ble_server = BLEDevice::createServer();
+    ble_server->setCallbacks(new ServerCB());
+
+    BLEService *server_service = ble_server->createService(UUID_UART);
+
+    while (1)
+    {
+        if (deviceConnected)
+        {
+            if(txValue_uart.size() > 0) {
+                // procesar tx_uart
+            }
+        }
+        else
+        {
+            if(txValue_uart.size() > 0) {
+                // aqui no ponemos nada, pues es temporal el uso de ble.
+            }
+        }
+    }
+}
+
+esp_err_t BLE_INIT() {
+    // iniciar servidor ble.
+    
+
+}
--- Turpial/BLE.h
+++ Turpial/BLE.h
@@ -10,5 +10,32 @@
  */
 #ifndef BLE_H
 #define BLE_H
+#include <Arduino.h>
+#include <string>
+
+std::string txValue_uart;
+std::string rxValue_uart;
+
+uint16_t server_mtu = 255;
+
+std::string server_name = "subid.locha.io";
+
+std::string UUID_UART = "";
+std::string TX_UART = "";
+std::string RX_UART = "";
+
+/**
+ * @brief 
+ * 
+ * @param params 
+ */
+void BLE_task(void *params);
+
+/**
+ * @brief 
+ * 
+ * @return esp_err_t 
+ */
+esp_err_t BLE_INIT();
 
 #endif // BLE_H
